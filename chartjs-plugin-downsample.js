5202344edcbe1ed8bd0c28357638fc6974d15814
Sean <seantdwilson@gmail.com>
2019-04-11 12:14:36 -0700

    chore: update built chartjs-plugin-downsample.js used for gh-pages

--- chartjs-plugin-downsample.js
+++ chartjs-plugin-downsample.js
@@ -1,5 +1,5 @@
 /* chartjs-plugin-downsample | AlbinoDrought | MIT License | https://github.com/AlbinoDrought/chartjs-plugin-downsample/blob/master/LICENSE | Based on flot-downsample, https://github.com/sveinn-steinarsson/flot-downsample/ */
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 },{}],2:[function(require,module,exports){
 var Chart = require('chart.js');
@@ -22,6 +22,9 @@ var defaultOptions = {
     restoreOriginalData: true,
     // if true, downsamples original data instead of data
     preferOriginalData: false,
+
+    //if not undefined and not empty, indicates the ids of the datasets to downsample
+    targetDatasets: [],
 
 };
 
@@ -103,12 +106,38 @@ function getOptions(chartInstance) {
     return chartInstance.options.downsample;
 }
 
+function getFilteredDatasets(chartInstance){
+    var targetDatasets = getOptions(chartInstance).targetDatasets;
+    var datasets = chartInstance.data.datasets;
+
+    if (targetDatasets.length === 0) {
+        return datasets;
+    }
+
+    var targetDatasetsMap = {};
+    for (var i = 0; i < targetDatasets.length; i++) {
+        var targetDataset = targetDatasets[i];
+        targetDatasetsMap[targetDataset] = true;
+    }
+
+    var filteredDatasets = [];
+    for (var i = 0; i < datasets.length; i++) {
+        var dataset = datasets[i];
+
+        if (targetDatasetsMap[dataset.id]) {
+            filteredDatasets.push(dataset);
+        }
+    }
+
+    return filteredDatasets;
+}
+
 function downsampleChart(chartInstance) {
     var options = getOptions(chartInstance),
         threshold = options.threshold;
     if(!options.enabled) return;
 
-    var datasets = chartInstance.data.datasets;
+    var datasets = getFilteredDatasets(chartInstance);
     for(var i = 0; i < datasets.length; i++) {
         var dataset = datasets[i];
 
@@ -151,7 +180,7 @@ var downsamplePlugin = {
         var options = getOptions(chartInstance);
         if(!options.enabled || !options.restoreOriginalData) return;
 
-        var datasets = chartInstance.data.datasets;
+        var datasets = getFilteredDatasets(chartInstance);
         for(var i = 0; i < datasets.length; i++) {
             var dataset = datasets[i];
 
